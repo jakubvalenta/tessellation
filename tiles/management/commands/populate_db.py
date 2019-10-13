be866383cbe4de607d3751c5b4784da8a7421649
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-14 01:51:22 +0200

    Add composition upload endpoint

--- tiles/management/commands/populate_db.py
+++ tiles/management/commands/populate_db.py
@@ -1,34 +1,36 @@
+import json
 import logging
+from base64 import b64encode
 from pathlib import Path
 
-from django.core.files.images import ImageFile
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from tiles.models import Composition, Image, Tile
+from tiles.serializers import CompositionSerializer
 
 logger = logging.getLogger(__name__)
 
-images_path = Path(__file__).parents[2] / 'fixtures' / 'images'
-data_images = [
-    {'name': 'tile-1.png', 'connections': [1, 2, 3, 1]},
-    {'name': 'tile-2.png', 'connections': [2, 1, 3, 3]},
-    {'name': 'tile-3.png', 'connections': [3, 1, 2, 2]},
-]
-data_tiles = [
-    {'imgId': 1, 'rotation': 1},
-    {'imgId': 1, 'rotation': 2},
-    {'imgId': 2, 'rotation': 0},
-    {'imgId': 2, 'rotation': 3},
-    {'imgId': 3, 'rotation': 3},
-    {'imgId': 1, 'rotation': 3},
-    {'imgId': 2, 'rotation': 2},
-    {'imgId': 3, 'rotation': 1},
-    {'imgId': 1, 'rotation': 0},
-    {'imgId': 3, 'rotation': 2},
-    {'imgId': 3, 'rotation': 0},
-    {'imgId': 2, 'rotation': 1},
-]
+fixtures_path = Path(__file__).parents[2] / 'fixtures'
+
+
+def create_base64_data_url(path: Path):
+    data = b64encode(path.read_bytes()).decode()
+    suffix = path.suffix[1:]
+    return f'data:text/{suffix};base64,{data}'
+
+
+def load_fixture(fixture_path: str):
+    logger.info('Loading fixture %s', fixture_path)
+    with fixture_path.open() as f:
+        data = json.load(f)
+    for image_data in data['images']:
+        image_path = fixtures_path / 'images' / image_data['url']
+        image_data['image'] = create_base64_data_url(image_path)
+    serializer = CompositionSerializer(data=data)
+    if not serializer.is_valid():
+        logger.error(serializer.errors)
+        return
+    serializer.save()
 
 
 class Command(BaseCommand):
@@ -36,22 +38,5 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        images = []
-        for row in data_images:
-            image_name = row['name']
-            logger.info('Creating Image %s %s', image_name, row['connections'])
-            image = Image.objects.create(connections=row['connections'])
-            image_f = (images_path / image_name).open('rb')
-            image.image.save(image_name, ImageFile(image_f))
-            image.save()
-            images.append(image)
-        tiles = []
-        for row in data_tiles:
-            tile = Tile.objects.create(
-                image=images[row['imgId'] - 1], rotation=row['rotation']
-            )
-            tile.save()
-            tiles.append(tile)
-        composition = Composition.objects.create(width=5, height=5)
-        composition.tiles.set(tiles)
-        composition.save()
+        for fixture_path in fixtures_path.glob('*.json'):
+            load_fixture(fixture_path)
--- tiles/serializers.py
+++ tiles/serializers.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
 
 from tiles.models import Composition, Image, Tile
@@ -5,25 +7,58 @@ from tiles.models import Composition, Image, Tile
 
 class ImageSerializer(serializers.ModelSerializer):
     imgId = serializers.CharField(source='pk', read_only=True)
+    imgRef = serializers.IntegerField(write_only=True)
     url = serializers.FileField(source='image', read_only=True, use_url=True)
+    image = Base64ImageField(write_only=True)
 
     class Meta:
         model = Image
-        fields = ['imgId', 'url', 'connections']
+        fields = ['imgId', 'imgRef', 'url', 'image', 'connections']
 
 
 class TileSerializer(serializers.ModelSerializer):
     imgId = serializers.CharField(source='image.pk', read_only=True)
+    imgRef = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Tile
-        fields = ['imgId', 'rotation']
+        fields = ['imgId', 'imgRef', 'rotation']
+
+
+class SizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Composition
+        fields = ['width', 'height']
 
 
 class CompositionSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True, read_only=True)
-    tiles = TileSerializer(many=True, read_only=True)
+    size = SizeSerializer()
+    images = ImageSerializer(many=True)
+    tiles = TileSerializer(many=True)
 
     class Meta:
         model = Composition
         fields = ['id', 'size', 'images', 'tiles']
+
+    @transaction.atomic
+    def create(self, validated_data: dict) -> Composition:
+        images = {
+            image_data['imgRef']: Image.objects.create(
+                image=image_data['image'],
+                connections=image_data['connections'],
+            )
+            for image_data in validated_data['images']
+        }
+        tiles = {
+            Tile.objects.create(
+                image=images[tile_data['imgRef']],
+                rotation=tile_data['rotation'],
+            )
+            for tile_data in validated_data['tiles']
+        }
+        composition = Composition.objects.create(
+            width=validated_data['size']['width'],
+            height=validated_data['size']['height'],
+        )
+        composition.tiles.set(tiles)
+        return composition
