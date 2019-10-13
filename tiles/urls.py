c272eb0983a7b5a60bdb1d2641335e347166e847
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-13 21:20:09 +0200

    Add index view

--- tiles/urls.py
+++ tiles/urls.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from tiles import views
+
+urlpatterns = [path('', views.index, name='index')] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
