2098ed80c2106801a95c91abbb77184ac1d47ea4
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-15 19:36:49 +0200

    Use SASS

--- conf/settings.py
+++ conf/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'debug_toolbar',
+    'sass_processor',
     'accounts',
     'tiles',
 ]
@@ -88,6 +89,12 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
 
 LOGGING = {
     'version': 1,
