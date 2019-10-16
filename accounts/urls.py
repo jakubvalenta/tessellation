df5f2e041715a8ef09b23298d1c97daf95d0184a
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-16 09:20:14 +0200

    Fix accounts redirects and restrict profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
+from accounts.views import ProfileView
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from django.views.generic import TemplateView
 
 app_name = 'accounts'
 urlpatterns = [
@@ -10,11 +10,7 @@ urlpatterns = [
         name='login',
     ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path(
-        'profile/',
-        TemplateView.as_view(template_name='profile.html'),
-        name='profile',
-    ),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path(
         'change-password/',
         auth_views.PasswordChangeView.as_view(
@@ -29,32 +25,32 @@ urlpatterns = [
         ),
         name='password_change_done',
     ),
-    path(
-        'reset-password/',
-        auth_views.PasswordResetView.as_view(
-            template_name='password_reset.html'
-        ),
-        name='password_reset',
-    ),
-    path(
-        'reset-password/done/',
-        auth_views.PasswordResetDoneView.as_view(
-            template_name='password_reset_done.html'
-        ),
-        name='password_reset_done',
-    ),
-    path(
-        'reset/<str:uidb64>/<str:token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='password_reset_confirm.html'
-        ),
-        name='password_reset_confirm',
-    ),
-    path(
-        'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name='password_reset_complete.html'
-        ),
-        name='password_reset_complete',
-    ),
+    # path(
+    #     'reset-password/',
+    #     auth_views.PasswordResetView.as_view(
+    #         template_name='password_reset.html'
+    #     ),
+    #     name='password_reset',
+    # ),
+    # path(
+    #     'reset-password/done/',
+    #     auth_views.PasswordResetDoneView.as_view(
+    #         template_name='password_reset_done.html'
+    #     ),
+    #     name='password_reset_done',
+    # ),
+    # path(
+    #     'reset/<str:uidb64>/<str:token>/',
+    #     auth_views.PasswordResetConfirmView.as_view(
+    #         template_name='password_reset_confirm.html'
+    #     ),
+    #     name='password_reset_confirm',
+    # ),
+    # path(
+    #     'reset/done/',
+    #     auth_views.PasswordResetCompleteView.as_view(
+    #         template_name='password_reset_complete.html'
+    #     ),
+    #     name='password_reset_complete',
+    # ),
 ]
--- conf/urls.py
+++ conf/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('tiles.urls')),
 ]
 
