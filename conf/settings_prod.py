166babc9cbee09eda42ffb1f460f998313b48c37
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-17 11:59:40 +0200

    settings_prod: Cache templates

--- conf/settings_prod.py
+++ conf/settings_prod.py
@@ -2,11 +2,20 @@ import os
 from pathlib import Path
 
 from conf.settings_base import *  # noqa: F401, F403
-from conf.settings_base import DATABASES
+from conf.settings_base import DATABASES, TEMPLATES
 
 DEBUG = False
-SECRET_KEY = Path(os.environ['DJANGO_SECRET_KEY_FILE']).read_bytes()
+SECRET_KEY = Path(os.environ['DJANGO_SECRET_KEY_FILE']).read_text().strip()
 DATABASES['default']['CONN_MAX_AGE'] = 300
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    (
+        'django.template.loaders.cached.Loader',
+        [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ],
+    )
+]
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
