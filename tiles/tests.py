0e08fa8c9e4e73d8c1965733e49c9334816a4ccc
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-17 12:15:57 +0200

    tests: Use ddt

--- tiles/tests.py
+++ tiles/tests.py
@@ -1,20 +1,17 @@
+from ddt import data, ddt, unpack
 from django.test import TestCase
 
 from tiles.models import find_shortest_str
 
 
+@ddt
 class TestModels(TestCase):
-    def test_find_shortest_str(self):
-        result = find_shortest_str('a', test_fn=lambda x: x not in ['x'])
-        result = find_shortest_str(
-            'abc', test_fn=lambda x: x not in ['a', 'x']
-        )
-        self.assertEqual(result, 'ab')
-        result = find_shortest_str(
-            'abc', test_fn=lambda x: x not in ['a', 'ab']
-        )
-        self.assertEqual(result, 'abc')
-        result = find_shortest_str(
-            'abc', test_fn=lambda x: x not in ['a', 'ab', 'abc']
-        )
-        self.assertIsNone(result)
+    @data(
+        ('abc', ['a', 'x'], 'ab'),
+        ('abc', ['a', 'ab'], 'abc'),
+        ('abc', ['a', 'ab', 'abc'], None),
+    )
+    @unpack
+    def test_find_shortest_str(self, s, existing_s, expected):
+        result = find_shortest_str(s, test_fn=lambda x: x not in existing_s)
+        self.assertEqual(result, expected)
