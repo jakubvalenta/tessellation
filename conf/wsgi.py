b8f4cb681063102deb24222917e80206f2865c4b
Jakub Valenta <jakub@jakubvalenta.cz>
2019-10-17 19:10:47 +0200

    wsgi: Define default secret key file

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -2,5 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings_prod_ssl')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings_prod')
+os.environ.setdefault('DJANGO_SECRET_KEY_FILE', '/run/keys/tiles-secret-key')
 application = get_wsgi_application()
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from tiles import __title__
 
 setup(
     name='tiles',
-    version='0.1.0',
+    version='0.1.1',
     description=__title__,
     long_description=(Path(__file__).parent / 'README.md').read_text(),
     url='https://lab.saloun.cz/jakub/tiles2',
