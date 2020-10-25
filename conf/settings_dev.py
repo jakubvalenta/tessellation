039f01995b6d14b21f6c418699644bc6425a4d07
Jakub Valenta <jakub@jakubvalenta.cz>
2020-10-25 14:44:18 +0100

    conf/settings_dev: Allow running on tessellation.local

--- conf/settings_dev.py
+++ conf/settings_dev.py
@@ -1,6 +1,7 @@
 from conf.settings_base import *  # noqa: F401, F403
 from conf.settings_base import INSTALLED_APPS, MIDDLEWARE, WEBPACK_LOADER
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.local']
 SECRET_KEY = 'lrtp%mw(ep03k_o94g@eq9b2nfj+xp_%c2ltj158q!$vuvekgm'
 DEBUG = True
 INSTALLED_APPS += ['debug_toolbar']
